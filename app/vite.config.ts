9dfbbf1f23afc5b05d135106aeff19b7a56b4fdf
qunqin <146629280+qwq202@users.noreply.github.com>
2026-05-07 09:05:18 +0800

    chore: disable build-time i18n translation

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import { createHtmlPlugin } from 'vite-plugin-html'
-import { createTranslationPlugin } from "./src/translator"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
     createHtmlPlugin({
       minify: true,
     }),
-    createTranslationPlugin(),
   ],
   resolve: {
     alias: {
